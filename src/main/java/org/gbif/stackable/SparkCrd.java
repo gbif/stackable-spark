f0db823dcca5eebd7d246c75b594c5cb02bad408
fmendezh <federicomh@gmail.com>
2023-10-24 09:57:27 +0200

    removing final from fields for easier use in Spring applications  as ConfigurationProperties

--- src/main/java/org/gbif/stackable/SparkCrd.java
+++ src/main/java/org/gbif/stackable/SparkCrd.java
@@ -36,13 +36,13 @@ public class SparkCrd implements ToBuilder {
 
   private static final ObjectMapper MAPPER = new ObjectMapper(new YAMLFactory());
 
-  @Builder.Default private final String apiVersion = "spark.stackable.tech/v1alpha1";
+  @Builder.Default private String apiVersion = "spark.stackable.tech/v1alpha1";
 
-  @Builder.Default private final String kind = "SparkApplication";
+  @Builder.Default private String kind = "SparkApplication";
 
-  private final Metadata metadata;
+  private Metadata metadata;
 
-  private final Spec spec;
+  private Spec spec;
 
   @SneakyThrows
   public String toYamlString() {
@@ -65,10 +65,10 @@ public class SparkCrd implements ToBuilder {
   public static class Metadata implements ToBuilder {
 
     /** Application name */
-    private final String name;
+    private String name;
 
     /** K8 namespace. */
-    private final String namespace;
+    private String namespace;
   }
 
   @Data
@@ -77,19 +77,19 @@ public class SparkCrd implements ToBuilder {
   public static class Spec implements ToBuilder {
 
     /** Pod overrides */
-    private final RoleGroups roleGroups;
+    private RoleGroups roleGroups;
 
     /** Application version */
-    private final String version;
+    private String version;
 
     /** cluster or client. Currently only cluster is supported. */
-    @Builder.Default private final String mode = "cluster";
+    @Builder.Default private String mode = "cluster";
 
     /**
      * User-supplied image containing spark-job dependencies that will be copied to the specified
      * volume mount.
      */
-    private final String image;
+    private String image;
 
     /**
      * Spark image which will be deployed to driver and executor pods, which must contain spark
@@ -97,54 +97,54 @@ public class SparkCrd implements ToBuilder {
      * docker.stackable.tech/stackable/spark-k8s:3.3.0-stackable0.3.0.
      */
     @Builder.Default
-    private final String sparkImage =
+    private String sparkImage =
         "docker.stackable.tech/stackable/spark-k8s:3.3.0-stackable0.1.0";
 
     /**
      * Optional Enum (one of Always, IfNotPresent or Never) that determines the pull policy of the
      * spark job image.
      */
-    private final String sparkImagePullPolicy;
+    private String sparkImagePullPolicy;
 
     /**
      * An optional list of references to secrets in the same namespace to use for pulling any of the
      * images used by a SparkApplication resource. Each reference has a single property (name) that
      * must contain a reference to a valid secret.
      */
-    private final List<String> sparkImagePullSecrets;
+    private List<String> sparkImagePullSecrets;
 
     /** The actual application file that will be called by spark-submit. */
-    private final String mainApplicationFile;
+    private String mainApplicationFile;
 
     /** The main class i.e. entry point for JVM artifacts */
-    private final String mainClass;
+    private String mainClass;
 
     /** Arguments passed directly to the job artifact. */
-    private final List<String> args;
+    private List<String> args;
 
     /** S3 connection specification. See the S3 resources for more details. */
-    private final String s3connection;
+    private String s3connection;
 
     /** A map of key/value strings that will be passed directly to spark-submit. */
-    private final Map<String, String> sparkConf;
+    private Map<String, String> sparkConf;
 
     /** Python resources. */
-    private final Deps deps;
+    private Deps deps;
 
     /** A list of volumes */
-    private final List<Volume> volumes;
+    private List<Volume> volumes;
 
     /** Job resources. */
-    private final Job job;
+    private Job job;
 
     /** Spark driver. */
-    private final Driver driver;
+    private Driver driver;
 
     /** Spark executors definition. */
-    private final Executor executor;
+    private Executor executor;
 
     /** Log file directory settings. */
-    private final LogFileDirectory logFileDirectory;
+    private LogFileDirectory logFileDirectory;
 
     @Data
     @Builder(toBuilder = true)
@@ -152,21 +152,21 @@ public class SparkCrd implements ToBuilder {
     public static class RoleGroups implements ToBuilder {
 
       @JsonProperty("default")
-      private final Default _default;
+      private Default _default;
 
       @Data
       @Builder(toBuilder = true)
       @Jacksonized
       public static class Default implements ToBuilder {
 
-        private final PodOverrides podOverrides;
+        private PodOverrides podOverrides;
 
         @Data
         @Builder(toBuilder = true)
         @Jacksonized
         public static class PodOverrides implements ToBuilder {
 
-          private final Metadata metadata;
+          private Metadata metadata;
 
           @Data
           @Builder(toBuilder = true)
@@ -174,7 +174,7 @@ public class SparkCrd implements ToBuilder {
           public static class Metadata implements ToBuilder {
 
             /** Labels to be added to the K8 Pod or custom resource. */
-            @Builder.Default private final Map<String, String> labels = Collections.emptyMap();
+            @Builder.Default private Map<String, String> labels = Collections.emptyMap();
           }
         }
       }
@@ -186,16 +186,16 @@ public class SparkCrd implements ToBuilder {
     public static class Deps implements ToBuilder {
 
       /** A list of python packages that will be installed via pip. */
-      private final List<String> requirements;
+      private List<String> requirements;
 
       /** A list of packages that is passed directly to spark-submit. */
-      private final List<String> packages;
+      private List<String> packages;
 
       /** A list of excluded packages that is passed directly to spark-submit */
-      private final List<String> excludePackages;
+      private List<String> excludePackages;
 
       /** A list of repositories that is passed directly to spark-submit */
-      private final List<String> repositories;
+      private List<String> repositories;
     }
 
     @Data
@@ -203,18 +203,18 @@ public class SparkCrd implements ToBuilder {
     @Jacksonized
     public static class ConfigMap implements ToBuilder {
 
-      private final String name;
+      private String name;
 
-      private final List<Item> items;
+      private List<Item> items;
 
       @Data
       @Builder(toBuilder = true)
       @Jacksonized
       public static class Item implements ToBuilder {
 
-        private final String key;
+        private String key;
 
-        private final String path;
+        private String path;
       }
     }
 
@@ -224,12 +224,12 @@ public class SparkCrd implements ToBuilder {
     public static class Volume implements ToBuilder {
 
       /** The volume name. */
-      private final String name;
+      private String name;
 
       /** The persistent volume claim backing the volume. */
-      private final PersistentVolumeClaim persistentVolumeClaim;
+      private PersistentVolumeClaim persistentVolumeClaim;
 
-      private final ConfigMap configMap;
+      private ConfigMap configMap;
 
       @Data
       @Builder(toBuilder = true)
@@ -237,7 +237,7 @@ public class SparkCrd implements ToBuilder {
       public static class PersistentVolumeClaim implements ToBuilder {
 
         /** The persistent volume claim name backing the volume. */
-        private final String claimName;
+        private String claimName;
       }
     }
 
@@ -247,23 +247,23 @@ public class SparkCrd implements ToBuilder {
     public static class Job implements ToBuilder {
 
       /** Resources specification for the initiating Job. */
-      private final Resources resources;
+      private Resources resources;
     }
 
     /** Mounted Volume. */
     @Data
     @Builder(toBuilder = true)
     @Jacksonized
-    public static final class VolumeMount implements ToBuilder {
+    public static class VolumeMount implements ToBuilder {
 
       /** Name of mount. */
-      private final String name;
+      private String name;
 
       /** Volume mount path. */
-      private final String mountPath;
+      private String mountPath;
 
       /** Volume mount sub-path. */
-      private final String subPath;
+      private String subPath;
     }
 
     @Data
@@ -271,79 +271,79 @@ public class SparkCrd implements ToBuilder {
     @Jacksonized
     public static class Resources implements ToBuilder {
 
-      private final Cpu cpu;
+      private Cpu cpu;
 
-      private final Memory memory;
+      private Memory memory;
 
       @Data
       @Builder(toBuilder = true)
       @Jacksonized
       public static class Cpu implements ToBuilder {
 
-        private final String min;
+        private String min;
 
-        private final String max;
+        private String max;
       }
 
       @Data
       @Builder(toBuilder = true)
       @Jacksonized
       public static class Memory implements ToBuilder {
-        private final String limit;
+        private String limit;
       }
     }
 
     @Data
     @Builder(toBuilder = true)
     @Jacksonized
-    public static final class Driver implements ToBuilder {
+    public static class Driver implements ToBuilder {
 
       /** Resources specification for the component Pod. */
-      private final Resources resources;
+      private Resources resources;
 
       /** A list of mounted volumes for the component Pod. */
-      private final List<VolumeMount> volumeMounts;
+      private List<VolumeMount> volumeMounts;
 
       /**
        * Driver Pod placement affinity. See <a
        * href="https://docs.stackable.tech/home/nightly/spark-k8s/usage-guide/pod-placement.html">Pod
        * placement</a>.
        */
-      private final String affinity;
+      private String affinity;
 
       /**
        * Logging aggregation for the driver Pod. See <a
        * href="https://docs.stackable.tech/home/nightly/concepts/logging.html">Logging</a>.
        */
-      private final String logging;
+      private String logging;
     }
 
     @Data
     @Builder(toBuilder = true)
     @Jacksonized
-    public static final class Executor implements ToBuilder {
+    public static class Executor implements ToBuilder {
 
       /** Number of executor instances launched for this job. */
-      private final int instances;
+      private int instances;
 
       /** Resources specification for the component Pod. */
-      private final Resources resources;
+      private Resources resources;
 
       /** A list of mounted volumes for the component Pod. */
-      private final List<VolumeMount> volumeMounts;
+      private List<VolumeMount> volumeMounts;
 
       /**
        * Driver Pod placement affinity. See <a
        * href="https://docs.stackable.tech/home/nightly/spark-k8s/usage-guide/pod-placement.html">Pod
        * placement</a>.
        */
-      private final String affinity;
+      private String affinity;
 
       /**
        * Logging aggregation for the driver Pod. See <a
        * href="https://docs.stackable.tech/home/nightly/concepts/logging.html">Logging</a>.
        */
-      private final String logging;
+      private String logging;
     }
 
     @Data
@@ -354,10 +354,10 @@ public class SparkCrd implements ToBuilder {
       /**
        * S3 bucket definition where applications should publish events for the Spark History server.
        */
-      private final String bucket;
+      private String bucket;
 
       /** Prefix to use when storing events for the Spark History server. */
-      private final String prefix;
+      private String prefix;
     }
   }
 }
